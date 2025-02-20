4c338218a6717bc448a99f436b0e81579928debe
Ankit yadav <ay870421@gmail.com>
2025-02-20 10:58:09 +0530

    mongo uri changed

--- backend/config/connectiondb.js
+++ backend/config/connectiondb.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+dotenv.config();
 const connectiondb = async () => {
   try {
-    const res = await mongoose.connect("mongodb://localhost:27017/collage");
+    const res = await mongoose.connect(process.env.MONGO_URL);
     if (res) {
       console.log("mongodb connection is successfull...",process.env.PORT );
     }
